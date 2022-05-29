53c0c2804d9dc23abd20b6068d65fe4bf794899e
Camroku <cinaryilmaz.gnu@gmail.com>
2022-05-29 18:09:29 +0300

    0.2.2/0.2.3

--- qo-prism.js
+++ qo-prism.js
@@ -1,12 +1,13 @@
 Prism.languages['qo'] = {
     'comment': /(\/\*[^\*\/]*\*\/)/s,
     'string': /("[^"]*"|'[^']*')/s,
-	'keyword': /(func|elif|if|else|return|foreach|for|while|times)/,
+	'keyword': /(func|elif|if|else|return|foreach|for|while|times|import|define)/,
     'number': /([0-9]+)/,
     'function': /([a-zA-Z0-9_]+)(?=\()/,
+    'important': /(__export__)/,
     'variable': /([a-zA-Z0-9_]+)/,
-	'builtin': /(println|print|input|toInt|toStr|toBool|type|exit|mod|None)/,
+	'builtin': /(println|print|input|toInt|toStr|toBool|type|exit|mod|None|exportAll)/,
     'boolean': /(True|False)/,
-    'operator': /(\+|-|\*|\/|=|<|>|<=|>=|==|!=|as|in|&|%|&&|\|\|)/,
+    'operator': /(\+|-|\*|\/|=|<|>|<=|>=|==|!=|as|in|&|%|&&|\|\|\+=)/,
     'punctuation': /(\(|\)|,|\[|\]|;|:|{|})/,
 };
