4256f4621a736d46c20769a41a45e61b25989a4d
Arnolxu <cinaryilmaz.gnu@gmail.com>
2022-08-05 17:56:33 +0300

    Update stuff

--- qo-prism.js
+++ qo-prism.js
@@ -1,13 +1,13 @@
 Prism.languages['qo'] = {
-    'comment': /(\/\*[^\*\/]*\*\/)/s,
+    'comment': /(\/\*[^\*\/]*\*\/)|(#.+[^$])/,
     'string': /("(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*')/s,
+	'builtin': /(?<![a-zA-Z0-9_])(println|print|input|exit|hasAttr|exists|remove|None|chr|len|__main__|__qcf__)(?![a-zA-Z0-9_])/,
 	'keyword': /(func|elif|if|else|return|foreach|for|while|times|include|define)/,
     'number': /([0-9]+)/,
     'function': /([a-zA-Z0-9_]+)(?=\()/,
     'important': /(__export__|(?=include )[a-zA-Z0-9_]+)/,
     'operator': /(\+|-|\*|\/|=|<|>|<=|>=|==|!=| as | in |&|%|&&|\|\|\+=|\*\*)/,
     'variable': /([a-zA-Z0-9_]+)/,
-	'builtin': /(println|print|input|toInt|toStr|toBool|type|exit|mod|hasAttr|exists|remove|None|exportAll|__main__|__qcf__)/,
     'boolean': /(True|False)/,
     'punctuation': /(\(|\)|,|\[|\]|;|:|{|}|.)/,
 };
