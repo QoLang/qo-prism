39fef5c90cc1aa68ddce3f76f66060465d2d9287
Arnolxu <cinaryilmaz.gnu@gmail.com>
2022-12-01 13:44:24 +0300

    Small details

--- qo-prism.js
+++ qo-prism.js
@@ -1,13 +1,13 @@
 Prism.languages['qo'] = {
     'comment': /(\/\*[^\*\/]*\*\/)|(#.+[^$])/,
-    'string': /("(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*')/s,
+    'string': /(%?"(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*')/,
     'builtin': /(?<![a-zA-Z0-9_])(print|println|input|exit|hasAttr|exists|remove|move|sleep|len|chr|ord|__main__|__qcf__|None)(?![a-zA-Z0-9_])/,
     'keyword': /(?<![a-zA-Z0-9_])(func|if|elif|else|for|while|times|return|foreach|include|define)(?![a-zA-Z0-9_])/,
     'number': /([0-9]+)/,
     'function': /([a-zA-Z0-9_]+)(?=\()/,
     'important': /(__export__|(?=include )[a-zA-Z0-9_]+)/,
     'operator': /(&&|\|\||\?\?|<|>|==|<=|>=|!=|\+|-|\*|\/|\*\*|%|=|\+=|-=|\*=|\/=|%=| as | in )/,
-    'variable': /([a-zA-Z0-9_]+)/,
+    'variable': /(&?[a-zA-Z0-9_]+)/,
     'boolean': /(True|False)/,
     'punctuation': /(\(|\)|,|\[|\]|;|:|{|}|.)/,
 };
